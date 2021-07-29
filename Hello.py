d632c74f1c8938a9cfad19179f3bc2bd3c1e0036
DESKTOP-PC3Q46Q\Hp <haris.masood@kavmail.net>
2021-07-29 13:05:36 +0500

    initialized

---
+++ Hello.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+class Hello:
+    def __init__ (self, name = 'haris'):
+        self.name = name
+    
+    def printing (self):
+        print(self.name)
---
+++ main.py
@@ -0,0 +1,8 @@
+import Hello
+
+class main:
+    h = Hello.Hello('A.R')
+    h.printing()
+    ha = Hello.Hello()
+    ha.printing()
+    
